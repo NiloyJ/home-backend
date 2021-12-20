f270d24e5703a53e5c39e23977e758e386c4faf2
NiloyJ <joydipbarua2004@gmail.com>
2021-12-20 14:53:07 +0600

    deploy back

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 // const port = 5000;
-const port = Process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 const uri = "mongodb+srv://dbUser:RAFJo96OGXCDyjYc@cluster0.baet0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
